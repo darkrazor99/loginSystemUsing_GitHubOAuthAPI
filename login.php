75661adf720d0a31e7023f41635d1e8118711726
dark_razor99 <tarektheman99@gmail.com>
2020-09-04 16:14:01 +0200

    edited login.php

--- login.php
+++ login.php
@@ -11,9 +11,11 @@
   <body>
     <form method="post">
       <label>username</label>
-      <input type="text" name="username" value="<?php echo (isset($_POST['username']) ? $_POST['username'] : ''); ?>">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 9px;" type="text" name="username" value="<?php echo (isset($_POST['username']) ? $_POST['username'] : ''); ?>">
+      <br>
       <label>password</label>
-      <input type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>">
+      <input style="margin-bottom: 10px; width: 200px; margin-left: 10px;" type="password" name="pwd" value="<?php echo (isset($_POST['pwd']) ? $_POST['pwd'] : ''); ?>">
+      <br>
       <button type="submit" name="login">login</button>
       <p><a href="signup.php?action=signup">signup with github</a></p>
     </form>
